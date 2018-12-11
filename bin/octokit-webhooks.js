0a3f83fb161affcaf82add851f8cfa05f0c2ce48
Gregor <gregor@martynus.net>
2018-12-11 10:47:06 -0800

    build: initial build script

---
+++ bin/octokit-webhooks.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const checkOrUpdateWebhooks = require('../lib/check-or-update-webhooks')
+
+const { cached, _: [command] } = require('yargs')
+  .command('update', 'Update webhooks', yargs => {
+    yargs
+      .options({
+        'cached': {
+          describe: 'Load HTML from local cache',
+          type: 'boolean',
+          default: false
+        }
+      })
+      .example('$0 update --cached')
+  })
+  .command('check', 'Check if webhooks are up-to-date', yargs => {
+    yargs
+      .options({
+        'cached': {
+          describe: 'Load HTML from local cache',
+          type: 'boolean',
+          default: false
+        }
+      })
+      .example('$0 check --cached')
+  })
+  .help('h')
+  .alias('h', ['help', 'usage'])
+  .demandCommand(1, '')
+  .usage('bin/octokit-webhooks.js <command> [--cached]')
+  .argv
+
+if (!['update', 'check'].includes(command)) {
+  console.log(`"${command}" must be one of: update, check`)
+  process.exit(1)
+}
+
+checkOrUpdateWebhooks({ cached, checkOnly: command === 'check' })
+  .catch(error => {
+    console.log(error.stack)
+    process.exit(1)
+  })
