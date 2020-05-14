45ae4bec7b6104c2003824f494747669d323a820
Zeke Sikelianos <zeke@sikelianos.com>
2020-05-14 13:33:06 -0700

    feat: event descriptions (#98)

---
+++ schema.js
@@ -0,0 +1,16 @@
+module.exports = {
+  properties: {
+    name: {
+      type: 'string'
+    },
+    description: {
+      type: 'string'
+    },
+    actions: {
+      type: 'array'
+    },
+    examples: {
+      type: 'array'
+    }
+  }
+}
