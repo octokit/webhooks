07dee0bddb18847a541f8fe306eed9bf66439f59
Egor Zaitsev <heuels@gmail.com>
2020-10-08 13:07:58 +0300

    chore: add `validate` script

--- bin/octokit-payloads.js
+++ bin/octokit-payloads.js
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const validate = require('../payload-schemas');
 
-fs.readdirSync('./payload-examples/api.github.com').forEach(filename => {
-  const [event, ...unused] = filename.split('.');
-  const fileBuffer = fs.readFileSync(`./payload-examples/api.github.com/${filename}`);
+const fs = require("fs");
+const validate = require("../payload-schemas");
+
+let hasErrors = false;
+const payloads = `./payload-examples/api.github.com`;
+
+fs.readdirSync(payloads).forEach((filename) => {
+  const [event, ...unused] = filename.split(".");
+  const fileBuffer = fs.readFileSync(`${payloads}/${filename}`);
   const file = JSON.parse(fileBuffer.toString());
 
   try {
     const validationResult = validate(event, file);
     if (!validationResult) {
       console.error(`❌ Payload '${filename}' does not match schema`);
+      hasErrors = true;
     } else {
       console.log(`✅ Payload '${filename}' matches schema`);
     }
   } catch (err) {
-    console.error(`Missing schema for event '${event}'`)
+    console.error(`Missing schema for event '${event}'`);
+    hasErrors = true;
   }
 });
+
+if (hasErrors) {
+  process.exit(1);
+}
