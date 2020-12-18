af90d67c00b79ab5cbbde999262809e164507350
Egor Zaitsev <heuels@gmail.com>
2020-12-18 10:18:12 +0300

    chore: fix formatting

--- bin/octokit-schema.js
+++ bin/octokit-schema.js
@@ -1,15 +1,14 @@
-const fs = require('fs');
-const $RefParser = require('@apidevtools/json-schema-ref-parser');
+const fs = require("fs");
+const $RefParser = require("@apidevtools/json-schema-ref-parser");
 
 async function run() {
   try {
-    const index = require('../payload-schemas/schemas/index.json');
+    const index = require("../payload-schemas/schemas/index.json");
     let schema = await $RefParser.dereference(index);
-    fs.writeFileSync('index.schema.json', JSON.stringify(schema, null, '  '));
+    fs.writeFileSync("index.schema.json", JSON.stringify(schema));
   } catch (err) {
     console.error(err);
   }
 }
 
-run()
-  // .then(() => console.log('DONE'))
+run();
