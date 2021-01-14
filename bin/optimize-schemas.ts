c9a5da9c85acd5ceaec0ac5f008881ba33800452
Gareth Jones <jones258@gmail.com>
2021-01-14 18:55:22 +1300

    feat: merge simple types in `oneOf`s

--- bin/optimize-schemas.ts
+++ bin/optimize-schemas.ts
@@ -2,13 +2,48 @@
 
 import { strict as assert } from "assert";
 import fs from "fs";
-import { JSONSchema7 } from "json-schema";
+import {
+  JSONSchema7,
+  JSONSchema7Definition,
+  JSONSchema7TypeName,
+} from "json-schema";
 import { format } from "prettier";
 
 const payloads = "payload-schemas/schemas";
 
+const isJustType = (
+  object: JSONSchema7Definition
+): object is Pick<Required<JSONSchema7>, "type"> => {
+  if (typeof object === "boolean") {
+    return false;
+  }
+
+  return Object.keys(object).length === 1 && object.type !== undefined;
+};
+
+const ensureArray = <T>(arr: T | T[]): T[] =>
+  Array.isArray(arr) ? arr : [arr];
+
 const isJsonSchemaObject = (object: unknown): object is JSONSchema7 =>
   typeof object === "object" && object !== null && !Array.isArray(object);
+
+const mergeSimpleTypes = (
+  objects: JSONSchema7Definition[]
+): JSONSchema7Definition[] => {
+  const simpleTypes = new Set<JSONSchema7TypeName>();
+
+  return objects
+    .filter((object) => {
+      if (isJustType(object)) {
+        ensureArray(object.type).forEach((type) => simpleTypes.add(type));
+
+        return false;
+      }
+
+      return true;
+    })
+    .concat({ type: Array.from(simpleTypes) });
+};
 
 fs.readdirSync(payloads).forEach((event) => {
   fs.readdirSync(`${payloads}/${event}`).forEach((schema) => {
@@ -31,9 +66,13 @@ fs.readdirSync(payloads).forEach((event) => {
             delete value.anyOf;
           }
 
-          // "oneOf" is redundant if it's only got one schema
-          if (value.oneOf && value.oneOf.length === 1) {
-            return value.oneOf[0];
+          if (value.oneOf) {
+            value.oneOf = mergeSimpleTypes(value.oneOf);
+
+            // "oneOf" is redundant if it's only got one schema
+            if (value.oneOf.length === 1) {
+              return value.oneOf[0];
+            }
           }
 
           return value;
