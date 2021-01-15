78c1e59755c8b6c06a66993e2a64b0142973d54b
Gareth Jones <jones258@gmail.com>
2021-01-15 15:54:53 +1300

    fix: add schemas for missing event actions

--- schema.d.ts
+++ schema.d.ts
@@ -197,6 +197,7 @@ export type RepositoryEvent =
   | RepositoryRenamedEvent
   | RepositoryTransferredEvent
   | RepositoryUnarchivedEvent;
+export type RepositoryDispatchEvent = RepositoryDispatchOnDemandTestEvent;
 export type RepositoryVulnerabilityAlertEvent =
   | RepositoryVulnerabilityAlertCreateEvent
   | RepositoryVulnerabilityAlertDismissEvent
@@ -224,6 +225,9 @@ export type TeamEvent =
   | TeamEditedEvent
   | TeamRemovedFromRepositoryEvent;
 export type WatchEvent = WatchStartedEvent;
+export type WorkflowRunEvent =
+  | WorkflowRunCompletedEvent
+  | WorkflowRunRequestedEvent;
 
 export interface CheckRunCompletedEvent {
   action: "completed";
@@ -8827,8 +8831,8 @@ export interface RepositoryUnarchivedEvent {
   installation?: Installation;
   organization?: Organization;
 }
-export interface RepositoryDispatchEvent {
-  action: string;
+export interface RepositoryDispatchOnDemandTestEvent {
+  action: "on-demand-test";
   branch: string;
   client_payload: {
     [k: string]: unknown;
@@ -9455,8 +9459,160 @@ export interface WorkflowDispatchEvent {
   organization: Organization;
   workflow: string;
 }
-export interface WorkflowRunEvent {
-  action: string;
+export interface WorkflowRunCompletedEvent {
+  action: "completed";
+  organization: Organization;
+  repository: Repository;
+  sender: User;
+  workflow: {
+    badge_url: string;
+    created_at: string;
+    html_url: string;
+    id: number;
+    name: string;
+    node_id: string;
+    path: string;
+    state: string;
+    updated_at: string;
+    url: string;
+  };
+  workflow_run: {
+    artifacts_url: string;
+    cancel_url: string;
+    check_suite_url: string;
+    conclusion: string | null;
+    created_at: string;
+    event: string;
+    head_branch: string;
+    head_commit: {
+      author: {
+        email: string;
+        name: string;
+      };
+      committer: {
+        email: string;
+        name: string;
+      };
+      id: string;
+      message: string;
+      timestamp: string;
+      tree_id: string;
+    };
+    head_repository: {
+      archive_url: string;
+      assignees_url: string;
+      blobs_url: string;
+      branches_url: string;
+      collaborators_url: string;
+      comments_url: string;
+      commits_url: string;
+      compare_url: string;
+      contents_url: string;
+      contributors_url: string;
+      deployments_url: string;
+      description: string;
+      downloads_url: string;
+      events_url: string;
+      fork: boolean;
+      forks_url: string;
+      full_name: string;
+      git_commits_url: string;
+      git_refs_url: string;
+      git_tags_url: string;
+      hooks_url: string;
+      html_url: string;
+      id: number;
+      issue_comment_url: string;
+      issue_events_url: string;
+      issues_url: string;
+      keys_url: string;
+      labels_url: string;
+      languages_url: string;
+      merges_url: string;
+      milestones_url: string;
+      name: string;
+      node_id: string;
+      notifications_url: string;
+      owner: User;
+      private: boolean;
+      pulls_url: string;
+      releases_url: string;
+      stargazers_url: string;
+      statuses_url: string;
+      subscribers_url: string;
+      subscription_url: string;
+      tags_url: string;
+      teams_url: string;
+      trees_url: string;
+      url: string;
+    };
+    head_sha: string;
+    html_url: string;
+    id: number;
+    jobs_url: string;
+    logs_url: string;
+    node_id: string;
+    pull_requests: unknown[];
+    repository: {
+      archive_url: string;
+      assignees_url: string;
+      blobs_url: string;
+      branches_url: string;
+      collaborators_url: string;
+      comments_url: string;
+      commits_url: string;
+      compare_url: string;
+      contents_url: string;
+      contributors_url: string;
+      deployments_url: string;
+      description: string;
+      downloads_url: string;
+      events_url: string;
+      fork: boolean;
+      forks_url: string;
+      full_name: string;
+      git_commits_url: string;
+      git_refs_url: string;
+      git_tags_url: string;
+      hooks_url: string;
+      html_url: string;
+      id: number;
+      issue_comment_url: string;
+      issue_events_url: string;
+      issues_url: string;
+      keys_url: string;
+      labels_url: string;
+      languages_url: string;
+      merges_url: string;
+      milestones_url: string;
+      name: string;
+      node_id: string;
+      notifications_url: string;
+      owner: User;
+      private: boolean;
+      pulls_url: string;
+      releases_url: string;
+      stargazers_url: string;
+      statuses_url: string;
+      subscribers_url: string;
+      subscription_url: string;
+      tags_url: string;
+      teams_url: string;
+      trees_url: string;
+      url: string;
+    };
+    rerun_url: string;
+    run_number: number;
+    status: string;
+    updated_at: string;
+    url: string;
+    workflow_id: number;
+    workflow_url: string;
+  };
+  installation?: Installation;
+}
+export interface WorkflowRunRequestedEvent {
+  action: "requested";
   organization: Organization;
   repository: Repository;
   sender: User;
