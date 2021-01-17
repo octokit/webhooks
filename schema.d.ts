80fb99b26e234fb940ee2f075d9c7d616c090bec
wolfy1339 <webmaster@wolfy1339.com>
2021-01-17 18:23:29 -0500

    build: generate types

--- schema.d.ts
+++ schema.d.ts
@@ -174,7 +174,10 @@ export type PullRequestEvent =
   | PullRequestUnassignedEvent
   | PullRequestUnlabeledEvent
   | PullRequestUnlockedEvent;
-export type PullRequestReviewEvent = PullRequestReviewSubmittedEvent;
+export type PullRequestReviewEvent =
+  | PullRequestReviewDismissedEvent
+  | PullRequestReviewEditedEvent
+  | PullRequestReviewSubmittedEvent;
 export type PullRequestReviewCommentEvent =
   | PullRequestReviewCommentCreatedEvent
   | PullRequestReviewCommentDeletedEvent
@@ -7053,6 +7056,369 @@ export interface PullRequestUnlockedEvent {
     received_events_url: string;
     type: string;
     site_admin: boolean;
+  };
+  repository: Repository;
+  installation?: Installation;
+  organization?: Organization;
+  sender: User;
+}
+export interface PullRequestReviewDismissedEvent {
+  action: "dismissed";
+  review: {
+    id: number;
+    node_id: string;
+    user: User;
+    body: string | null;
+    commit_id: string;
+    submitted_at: string;
+    state: string;
+    html_url: string;
+    pull_request_url: string;
+    author_association:
+      | "COLLABORATOR"
+      | "CONTRIBUTOR"
+      | "FIRST_TIMER"
+      | "FIRST_TIME_CONTRIBUTOR"
+      | "MANNEQUIN"
+      | "MEMBER"
+      | "NONE"
+      | "OWNER";
+    _links: {
+      html: {
+        href: string;
+      };
+      pull_request: {
+        href: string;
+      };
+    };
+  };
+  pull_request: {
+    url: string;
+    id: number;
+    node_id: string;
+    html_url: string;
+    diff_url: string;
+    patch_url: string;
+    issue_url: string;
+    number: number;
+    state: "open" | "closed";
+    locked: boolean;
+    title: string;
+    user: User;
+    body: string;
+    created_at: string;
+    updated_at: string;
+    closed_at: string | null;
+    merged_at: string | null;
+    merge_commit_sha: string | null;
+    assignee: User | null;
+    assignees: User[];
+    requested_reviewers: (User | Team)[];
+    requested_teams: Team[];
+    labels: {
+      id: number;
+      node_id: string;
+      url: string;
+      name: string;
+      color: string;
+      default: boolean;
+    }[];
+    milestone: {
+      url: string;
+      html_url: string;
+      labels_url: string;
+      id: number;
+      node_id: string;
+      number: number;
+      title: string;
+      description: string;
+      creator: User;
+      open_issues: number;
+      closed_issues: number;
+      state: string;
+      created_at: string;
+      updated_at: string;
+      due_on: string;
+      closed_at: string | null;
+    } | null;
+    draft: boolean;
+    commits_url: string;
+    review_comments_url: string;
+    review_comment_url: string;
+    comments_url: string;
+    statuses_url: string;
+    head: {
+      label: string;
+      ref: string;
+      sha: string;
+      user: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        followers_url: string;
+        following_url: string;
+        gists_url: string;
+        starred_url: string;
+        subscriptions_url: string;
+        organizations_url: string;
+        repos_url: string;
+        events_url: string;
+        received_events_url: string;
+        type: string;
+        site_admin: boolean;
+      };
+      repo: Repository;
+    };
+    base: {
+      label: string;
+      ref: string;
+      sha: string;
+      user: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        followers_url: string;
+        following_url: string;
+        gists_url: string;
+        starred_url: string;
+        subscriptions_url: string;
+        organizations_url: string;
+        repos_url: string;
+        events_url: string;
+        received_events_url: string;
+        type: string;
+        site_admin: boolean;
+      };
+      repo: Repository;
+    };
+    _links: {
+      self: {
+        href: string;
+      };
+      html: {
+        href: string;
+      };
+      issue: {
+        href: string;
+      };
+      comments: {
+        href: string;
+      };
+      review_comments: {
+        href: string;
+      };
+      review_comment: {
+        href: string;
+      };
+      commits: {
+        href: string;
+      };
+      statuses: {
+        href: string;
+      };
+    };
+    author_association:
+      | "COLLABORATOR"
+      | "CONTRIBUTOR"
+      | "FIRST_TIMER"
+      | "FIRST_TIME_CONTRIBUTOR"
+      | "MANNEQUIN"
+      | "MEMBER"
+      | "NONE"
+      | "OWNER";
+    active_lock_reason: "resolved" | "off-topic" | "too heated" | "spam" | null;
+  };
+  repository: Repository;
+  installation?: Installation;
+  organization?: Organization;
+  sender: User;
+}
+export interface PullRequestReviewEditedEvent {
+  action: "edited";
+  changes: {
+    body: {
+      from: string;
+    };
+  };
+  review: {
+    id: number;
+    node_id: string;
+    user: User;
+    body: string | null;
+    commit_id: string;
+    submitted_at: string;
+    state: string;
+    html_url: string;
+    pull_request_url: string;
+    author_association:
+      | "COLLABORATOR"
+      | "CONTRIBUTOR"
+      | "FIRST_TIMER"
+      | "FIRST_TIME_CONTRIBUTOR"
+      | "MANNEQUIN"
+      | "MEMBER"
+      | "NONE"
+      | "OWNER";
+    _links: {
+      html: {
+        href: string;
+      };
+      pull_request: {
+        href: string;
+      };
+    };
+  };
+  pull_request: {
+    url: string;
+    id: number;
+    node_id: string;
+    html_url: string;
+    diff_url: string;
+    patch_url: string;
+    issue_url: string;
+    number: number;
+    state: "open" | "closed";
+    locked: boolean;
+    title: string;
+    user: User;
+    body: string;
+    created_at: string;
+    updated_at: string;
+    closed_at: string | null;
+    merged_at: string | null;
+    merge_commit_sha: string | null;
+    assignee: User | null;
+    assignees: User[];
+    requested_reviewers: (User | Team)[];
+    requested_teams: Team[];
+    labels: {
+      id: number;
+      node_id: string;
+      url: string;
+      name: string;
+      color: string;
+      default: boolean;
+    }[];
+    milestone: {
+      url: string;
+      html_url: string;
+      labels_url: string;
+      id: number;
+      node_id: string;
+      number: number;
+      title: string;
+      description: string;
+      creator: User;
+      open_issues: number;
+      closed_issues: number;
+      state: string;
+      created_at: string;
+      updated_at: string;
+      due_on: string;
+      closed_at: string | null;
+    } | null;
+    draft: boolean;
+    commits_url: string;
+    review_comments_url: string;
+    review_comment_url: string;
+    comments_url: string;
+    statuses_url: string;
+    head: {
+      label: string;
+      ref: string;
+      sha: string;
+      user: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        followers_url: string;
+        following_url: string;
+        gists_url: string;
+        starred_url: string;
+        subscriptions_url: string;
+        organizations_url: string;
+        repos_url: string;
+        events_url: string;
+        received_events_url: string;
+        type: string;
+        site_admin: boolean;
+      };
+      repo: Repository;
+    };
+    base: {
+      label: string;
+      ref: string;
+      sha: string;
+      user: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        followers_url: string;
+        following_url: string;
+        gists_url: string;
+        starred_url: string;
+        subscriptions_url: string;
+        organizations_url: string;
+        repos_url: string;
+        events_url: string;
+        received_events_url: string;
+        type: string;
+        site_admin: boolean;
+      };
+      repo: Repository;
+    };
+    _links: {
+      self: {
+        href: string;
+      };
+      html: {
+        href: string;
+      };
+      issue: {
+        href: string;
+      };
+      comments: {
+        href: string;
+      };
+      review_comments: {
+        href: string;
+      };
+      review_comment: {
+        href: string;
+      };
+      commits: {
+        href: string;
+      };
+      statuses: {
+        href: string;
+      };
+    };
+    author_association:
+      | "COLLABORATOR"
+      | "CONTRIBUTOR"
+      | "FIRST_TIMER"
+      | "FIRST_TIME_CONTRIBUTOR"
+      | "MANNEQUIN"
+      | "MEMBER"
+      | "NONE"
+      | "OWNER";
+    active_lock_reason: "resolved" | "off-topic" | "too heated" | "spam" | null;
   };
   repository: Repository;
   installation?: Installation;
