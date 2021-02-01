23876b2dee40dc9555c0997c92b51256113aa1cc
Gareth Jones <Jones258@Gmail.com>
2021-02-01 14:18:48 +1300

    fix: add missing properties to `User` interface (#346)

--- schema.d.ts
+++ schema.d.ts
@@ -422,6 +422,8 @@ export interface User {
   login: string;
   id: number;
   node_id: string;
+  name?: string;
+  email?: string;
   avatar_url: string;
   gravatar_id: string;
   url: string;
@@ -456,28 +458,7 @@ export interface Repository {
    * Whether the repository is private or public.
    */
   private: boolean;
-  owner: {
-    login: string;
-    id: number;
-    name?: string | null;
-    email?: string | null;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-  };
+  owner: User;
   html_url: string;
   description: string | null;
   fork: boolean;
