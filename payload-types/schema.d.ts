92b2b3921bdca4020294bf505758a32f254412bd
wolfy1339 <webmaster@wolfy1339.com>
2021-05-24 09:10:13 -0400

    fix: add missing properties to `invitation` object in `organization#member_invited`

--- payload-types/schema.d.ts
+++ payload-types/schema.d.ts
@@ -3551,7 +3551,20 @@ export interface OrganizationMemberInvitedEvent {
   /**
    * The invitation for the user or email if the action is `member_invited`.
    */
-  invitation: {};
+  invitation: {
+    id: number;
+    node_id: string;
+    login: string;
+    email: string | null;
+    role: string;
+    created_at: string;
+    failed_at: string | null;
+    failed_reason: string | null;
+    inviter: User;
+    team_count: number;
+    invitation_teams_url: string;
+  };
+  user: User;
   sender: User;
   installation?: InstallationLite;
   organization: Organization;
