4f427e0eb03798484ca0a74ad4782a8ed1b8eba9
wolfy1339 <webmaster@wolfy1339.com>
2021-07-29 21:32:26 -0400

    fix: adapt  `getSections()` to newest docs layout

--- lib/get-sections.ts
+++ lib/get-sections.ts
@@ -5,7 +5,7 @@ import { Section } from ".";
 export const getSections = (html: string): Section[] => {
   const $ = cheerio.load(html);
 
-  const sections = $(".article-grid-toc-content ul a")
+  const sections = $("#in-this-article").next("ul").find("a")
     .map((index, element) => {
       const url = $(element).attr("href");
 
