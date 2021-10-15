2281de81297debdeb62a7f18c90693b40d55ff68
wolfy1339 <webmaster@wolfy1339.com>
2021-10-15 17:41:41 -0400

    fix: `check_suite#after` can be `null`

--- payload-types/schema.d.ts
+++ payload-types/schema.d.ts
@@ -1262,7 +1262,7 @@ export interface CheckSuiteCompletedEvent {
      */
     url: string;
     before: string | null;
-    after: string;
+    after: string | null;
     /**
      * An array of pull requests that match this check suite. A pull request matches a check suite if they have the same `head_sha` and `head_branch`. When the check suite's `head_branch` is in a forked repository it will be `null` and the `pull_requests` array will be empty.
      */
@@ -1339,7 +1339,7 @@ export interface CheckSuiteRequestedEvent {
      */
     url: string;
     before: string | null;
-    after: string;
+    after: string | null;
     /**
      * An array of pull requests that match this check suite. A pull request matches a check suite if they have the same `head_sha` and `head_branch`. When the check suite's `head_branch` is in a forked repository it will be `null` and the `pull_requests` array will be empty.
      */
@@ -1393,7 +1393,7 @@ export interface CheckSuiteRerequestedEvent {
      */
     url: string;
     before: string | null;
-    after: string;
+    after: string | null;
     /**
      * An array of pull requests that match this check suite. A pull request matches a check suite if they have the same `head_sha` and `head_branch`. When the check suite's `head_branch` is in a forked repository it will be `null` and the `pull_requests` array will be empty.
      */
