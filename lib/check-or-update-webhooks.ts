e2e1c391413a08786cdba45f20af919d34287fff
wolfy1339 <webmaster@wolfy1339.com>
2022-06-28 19:16:47 -0400

    lib: remove outdated GHES versions

--- lib/check-or-update-webhooks.ts
+++ lib/check-or-update-webhooks.ts
@@ -34,7 +34,7 @@ export const checkOrUpdateWebhooks = async ({
   }
 
   if (ghe === "") {
-    const gheVersions = ["2.21", "2.22", "3.0", "3.1", "3.2", "3.3"];
+    const gheVersions = ["3.1", "3.2", "3.3"];
 
     for (let gheVersion of gheVersions) {
       await checkOrUpdateWebhooks({
