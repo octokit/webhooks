b4ccecf9fdec17acda690f17a949bd887db2422c
wolfy1339 <webmaster@wolfy1339.com>
2022-08-09 16:31:21 -0400

    fix: add missing `additionalProperties`

--- payload-types/schema.d.ts
+++ payload-types/schema.d.ts
@@ -826,7 +826,6 @@ export interface BranchProtectionRuleEditedEvent {
     };
     required_deployments_enforcement_level?: {
       from: "off" | "non_admins" | "everyone";
-      [k: string]: unknown;
     };
     required_status_checks?: {
       from: string[];
