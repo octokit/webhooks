424413da83c59c688747d0ff3e82c70c84f80284
wolfy1339 <webmaster@wolfy1339.com>
2023-01-13 11:46:34 -0500

    feat: new `installation_target` event

--- payload-types/schema.d.ts
+++ payload-types/schema.d.ts
@@ -24,6 +24,7 @@ export type Schema =
   | GollumEvent
   | InstallationEvent
   | InstallationRepositoriesEvent
+  | InstallationTargetEvent
   | IssueCommentEvent
   | IssuesEvent
   | LabelEvent
@@ -142,6 +143,7 @@ export type InstallationEvent =
 export type InstallationRepositoriesEvent =
   | InstallationRepositoriesAddedEvent
   | InstallationRepositoriesRemovedEvent;
+export type InstallationTargetEvent = InstallationTargetRenamedEvent;
 export type IssueCommentEvent =
   | IssueCommentCreatedEvent
   | IssueCommentDeletedEvent
@@ -3592,6 +3594,62 @@ export interface InstallationRepositoriesRemovedEvent {
   }[];
   requester: User | null;
   sender: User;
+}
+/**
+ * Somebody renamed the user or organization account that a GitHub App is installed on.
+ */
+export interface InstallationTargetRenamedEvent {
+  changes: {
+    login?: {
+      from: string;
+    };
+    slug?: {
+      from: string;
+    };
+  };
+  action: "renamed";
+  account: {
+    avatar_url: string;
+    created_at?: string;
+    description?: null;
+    events_url?: string;
+    followers?: number;
+    followers_url?: string;
+    following?: number;
+    following_url?: string;
+    gists_url?: string;
+    gravatar_id?: string;
+    has_organization_projects?: boolean;
+    has_repository_projects?: boolean;
+    hooks_url?: string;
+    html_url: string;
+    id: number;
+    is_verified?: boolean;
+    issues_url?: string;
+    login?: string;
+    members_url?: string;
+    name?: string;
+    node_id: string;
+    organizations_url?: string;
+    public_gists?: number;
+    public_members_url?: string;
+    public_repos?: number;
+    received_events_url?: string;
+    repos_url?: string;
+    site_admin?: boolean;
+    slug?: string;
+    starred_url?: string;
+    subscriptions_url?: string;
+    type?: "Bot" | "User" | "Organization";
+    updated_at?: string;
+    url?: string;
+    website_url?: null;
+  };
+  repository?: Repository;
+  sender?: User;
+  installation: InstallationLite;
+  organization?: Organization;
+  target_type: string;
 }
 export interface IssueCommentCreatedEvent {
   action: "created";
@@ -7930,6 +7988,7 @@ export interface EventPayloadMap {
   gollum: GollumEvent;
   installation: InstallationEvent;
   installation_repositories: InstallationRepositoriesEvent;
+  installation_target: InstallationTargetEvent;
   issue_comment: IssueCommentEvent;
   issues: IssuesEvent;
   label: LabelEvent;
